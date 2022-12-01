0f9ae9ec84324497bc70d33b23d43f8cac2bc635
ramyar omed ali <ro21090@auis.edu.krd>
2022-12-01 23:03:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  dodger.addEventListener("keydown", function(e) {
+    if(e.key==="Arrowleft") {
+        moveDodgerLeft
+    }
+  })
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    if(left>0) {
+      dodger.style.left = `${left + 1}px`
+    }
+}
+dodger.addEventListener("keyup", function(e) {
+  if(e.key==="Arrowleft") {
+    moveDodgerRight
+  }
+})
